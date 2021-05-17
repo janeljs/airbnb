dc37af118317e485100fe222164562dab4f496cb
zombietux <>
2021-05-17 17:56:45 +0900

    feat: storyboard Tab bar 추가 (#3)

---
+++ iOS/airbnbApp/airbnbApp/Presentation/Login/ViewControllers/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  airbnbApp
+//
+//  Created by zombietux on 2021/05/17.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ iOS/airbnbApp/airbnbApp/Presentation/WishList/ViewControllers/WishListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WishListViewController.swift
+//  airbnbApp
+//
+//  Created by zombietux on 2021/05/17.
+//
+
+import UIKit
+
+class WishListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
